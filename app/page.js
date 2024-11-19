84e40b6f98b34adb338b7eddb9096d939a948299
Telly-McGuire <ryandiovenaguilar@gmail.com>
2024-11-19 18:27:59 +0800

    Updated

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 "use client";
 import Image from "next/image";
 import { useState } from "react";
-import 'app\globals.css';
 
 export default function Home() {
   const [task, setTask] = useState("");
